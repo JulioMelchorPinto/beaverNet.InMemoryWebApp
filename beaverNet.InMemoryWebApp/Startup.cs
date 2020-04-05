6653ca72551764c524e1a74bd15ed68b14b72b1c
Julio César Melchor Pinto <julio.melchor@ciencias.unam.mx>
2020-04-04 19:54:56 -0600

    User registration in memory database

--- beaverNet.InMemoryWebApp/Startup.cs
+++ beaverNet.InMemoryWebApp/Startup.cs
@@ -27,13 +27,15 @@ namespace beaverNet.InMemoryWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            /* services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("DefaultConnection"))); */
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase(databaseName: "app"));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
-           services.AddRazorPages();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
